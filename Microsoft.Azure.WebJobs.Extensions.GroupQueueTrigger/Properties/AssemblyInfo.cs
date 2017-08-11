9153894d3a01925efd018b4f1e1653e551162f23
Matias Quaranta <ealsur@hotmail.com>
2017-08-11 16:29:01 -0300

    Updating to new package generation

--- Microsoft.Azure.WebJobs.Extensions.GroupQueueTrigger/Properties/AssemblyInfo.cs
+++ Microsoft.Azure.WebJobs.Extensions.GroupQueueTrigger/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.Azure.WebJobs.Extensions.GroupQueueTrigger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Azure Webjobs SDK Extension for Triggering a Group of Messages")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.Azure.WebJobs.Extensions.GroupQueueTrigger")]
